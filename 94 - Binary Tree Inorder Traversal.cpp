2b0fb6ce381a9d7e03542a0c91a00f9797dffc7d
Nenshiv <9.nency@gmail.com>
2026-02-20 23:52:21 +0530

    feat: implement iterative inorder traversal using stack

---
+++ 94 - Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std; 
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+        TreeNode* current = root;
+
+        while (current != nullptr || !st.empty()) {
+
+            while (current != nullptr) {
+                st.push(current);
+                current = current->left;
+            }
+
+            current = st.top();
+            st.pop();
+            result.push_back(current->val);
+
+            current = current->right;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    Solution sol;
+    vector<int> result = sol.inorderTraversal(root);
+    for (int val : result) {
+        cout << val << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
